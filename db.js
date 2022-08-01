41bd1f01aa35f16f408f743c9ad914cd7c373652
PlayingSpree <winingame@hotmail.com>
2022-08-01 22:02:26 +0700

    Fix SSL

--- db.js
+++ db.js
@@ -2,7 +2,9 @@ const { Client } = require('pg');
 
 const client = new Client({
     connectionString: process.env.DATABASE_URL,
-    ssl: false
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 client.connect();
