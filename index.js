08961bfce341c92030c460393e54984811a1aa64
PlayingSpree <winingame@hotmail.com>
2022-10-02 20:44:25 +0700

    logs

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.get("/", (req, res) => {
 
 app.post("/webhook", function (req, res) {
     res.send("HTTP POST request sent to the webhook URL!")
+    console.log(JSON.stringify(req))
     // If the user sends a message to your bot, send a reply message
     if (req.body.events[0].type === "message" && req.body.events[0].message.type === "text") {
         // Read message
